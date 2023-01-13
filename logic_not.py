d630f17e36ff44fef40b2e84c85634df064e087c
jaikmthomas <jaikmthomas@gmail.com>
2023-01-13 20:32:39 +0530

    program to determine Grades scored in an exam

--- logic_not.py
+++ logic_not.py
@@ -1,7 +1,12 @@
-#write a program to determine that a person is an adult (18+) or a minor
-num = int(input("Write your age:"))
-
-if num < 18:
-    print("you are a minor")
-elif num >= 18:
-    print("you are an adult")
+#write a program to determine Grades scored in an exam
+a = int(input("Write your score:"))
+if a>= 90:
+    print("Great job! you got A grade")
+elif a<90 and a>=80:
+    print("Impressive! you got B grade")
+elif a<80 and a>=70:
+    print("Good! you got C grade")
+elif a<70 and a>=60:
+    print("you got D grade, Need to improve")
+else:
+    print("You are failed! you got E grade")
