cac823f72c3e77827837208415a950c27b5ca9b3
jaikmthomas <jaikmthomas@gmail.com>
2023-01-13 19:38:33 +0530

    program to determine that a person is an adult (18+) or a minor

--- logic_not.py
+++ logic_not.py
@@ -1,9 +1,7 @@
-#write a program to determine if a number is +ve, -ve, or 0
-num = int(input("write a number"))
+#write a program to determine that a person is an adult (18+) or a minor
+num = int(input("Write your age:"))
 
-if num > 0:
-    print("number is +ve")
-elif num < 0:
-    print("number is -ve")
-else:
-    print("number is 0")
+if num < 18:
+    print("you are a minor")
+elif num >= 18:
+    print("you are an adult")
