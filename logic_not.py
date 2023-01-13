eb203738b3ed7b557d332c79a34c4987a90f3cfc
jaikmthomas <jaikmthomas@gmail.com>
2023-01-13 19:25:05 +0530

    a program to determine if a number is +ve, -ve, or 0

--- logic_not.py
+++ logic_not.py
@@ -1,4 +1,9 @@
-is_hungry = False
-is_thirsty = True
-if is_thirsty and not is_hungry :
-    print("drink not eat")
+#write a program to determine if a number is +ve, -ve, or 0
+num = int(input("write a number"))
+
+if num > 0:
+    print("number is +ve")
+elif num < 0:
+    print("number is -ve")
+else:
+    print("number is 0")
