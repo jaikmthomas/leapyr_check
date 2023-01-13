500d7fd984c3b0ddbf2b2eb6c90947947e5a8da5
jaikmthomas <jaikmthomas@gmail.com>
2023-01-13 21:33:19 +0530

    to check leap year or not

--- logic_not.py
+++ logic_not.py
@@ -1,7 +1,7 @@
 #write a program to determine whether you have attendance to qualify the course
 a = int(input("Write the total number of classes held:"))
 b = int(input("number of classes you attended:"))
-c = (b/a)*100
+c = round((b/a)*100, 2)
 print(str(c)+"%")
 if c<75.0:
     print("You failed the course")
